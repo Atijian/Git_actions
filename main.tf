0be00b1c4deb4dbe771cefbf6d50ecf09ab8e310
Atijian <legitcydoc@gmail.com>
2025-01-15 16:56:05 +0530

    atishay

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-name-12345"
-  acl    = "private"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
